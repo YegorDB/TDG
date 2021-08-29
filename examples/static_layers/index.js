aede2400a131707aa100d9e818ecc438eadacfbd
Yegor Bitensky <yegorbitensky@gmail.com>
2021-08-29 21:56:09 +0300

    Update examples

--- examples/static_layers/index.js
+++ examples/static_layers/index.js
@@ -3,42 +3,42 @@ const screen = new TDG.screen.Screen('screen-id', {
 });
 
 
-let backgroundLayer = new TDG.layers.canvas.CanvasLayer();
-screen.addLayer('bg', backgroundLayer);
-screen.layers.bg.ctx.strokeStyle = '#8ecae6';
-screen.layers.bg.ctx.beginPath();
-let d = 6;
-for (let i = d; i <= 300; i += d) {
-  screen.layers.bg.ctx.moveTo(0, i);
-  screen.layers.bg.ctx.lineTo(i, 0);
-  screen.layers.bg.ctx.moveTo(300, i);
-  screen.layers.bg.ctx.lineTo(i, 300);
-  screen.layers.bg.ctx.moveTo(i - d, 0);
-  screen.layers.bg.ctx.lineTo(300, 300 + d - i);
-  screen.layers.bg.ctx.moveTo(0, i - d);
-  screen.layers.bg.ctx.lineTo(300 + d - i, 300);
-}
-screen.layers.bg.ctx.stroke();
+let backgroundLayer = screen.createCanvasLayer('bg', {
+  drawFunct: (ctx) => {
+    ctx.strokeStyle = '#8ecae6';
+    ctx.beginPath();
+    let d = 6;
+    for (let i = d; i <= 300; i += d) {
+      ctx.moveTo(0, i);
+      ctx.lineTo(i, 0);
+      ctx.moveTo(300, i);
+      ctx.lineTo(i, 300);
+      ctx.moveTo(i - d, 0);
+      ctx.lineTo(300, 300 + d - i);
+      ctx.moveTo(0, i - d);
+      ctx.lineTo(300 + d - i, 300);
+    }
+    ctx.stroke();
+  },
+});
 
 
-let backLayer = new TDG.layers.svg.SVGLayer();
-screen.addLayer('back', backLayer);
-let blueCircle = new TDG.items.svg.SVGItem('circle', {
+let backLayer = screen.createSVGLayer('back');
+let blueCircle = screen.layers.back.createItem('blueCircle', 'circle', {
   cx: 130,
   cy: 140,
   r: 120,
   fill: '#219ebc',
+  stroke: 'none',
 });
-screen.layers.back.addItem('blueCircle', blueCircle);
 
 
-let frontLayer = new TDG.layers.svg.SVGLayer();
-screen.addLayer('front', frontLayer);
-let yellowRectangle = new TDG.items.svg.SVGItem('rect', {
+let frontLayer = screen.createSVGLayer('front');
+let yellowRectangle = screen.layers.front.createItem('yellowRectangle', 'rect', {
   x: 150,
   y: 200,
   width: 120,
   height: 80,
   fill: '#ffb703',
+  stroke: 'none',
 });
-screen.layers.front.addItem('yellowRectangle', yellowRectangle);
