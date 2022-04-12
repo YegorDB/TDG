4a53afddf9cdb13ec2d35e81cfb9fb0746e0d4fe
Yegor Bitensky <yegorbitensky@gmail.com>
2022-04-12 23:51:48 +0300

    Move create SVG items functional to items manager class

--- src/layers/svg/main.js
+++ src/layers/svg/main.js
@@ -13,10 +13,7 @@ limitations under the License.
 
 
 import { BaseLayer } from '../base.js';
-import { Dimensions, UTILS } from '../../base.js';
-import {
-  SVGCircle, SVGEllipse, SVGItem, SVGPath, SVGPolygon, SVGPolyline, SVGText,
-} from '../../items/svg/main.js';
+import { UTILS } from '../../base.js';
 import { SVGItemsManager } from '../../managers.js';
 
 
@@ -27,107 +24,6 @@ class SVGLayer extends BaseLayer {
   constructor() {
     super();
     this.items = new SVGItemsManager(this);
-  }
-
-  /**
-   * Create item.
-   * @param {string} name - Item name.
-   * @param {string} elementName - SVG element name.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createItem(name, elementName, attrs) {
-    let item = new SVGItem(elementName, attrs);
-    this.items.add(name, item);
-    return item;
-  }
-
-  /**
-   * Create circle.
-   * @param {string} name - Item name.
-   * @param {Point|number[]} centre - Point instatce or (x, y) pair.
-   * @param {number} radius - Radius.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createCircle(name, centre, radius, attrs) {
-    let circle = new SVGCircle(centre, radius, attrs);
-    this.items.add(name, circle);
-    return circle;
-  }
-
-  /**
-   * Create ellipse.
-   * @param {string} name - Item name.
-   * @param {Point|number[]} centre - Point instatce or (x, y) pair.
-   * @param {EllipseRadiuses|number[]} radiuses - EllipseRadiuses instatce or (r1, r2) pair.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createEllipse(name, centre, radiuses, attrs) {
-    let ellipse = new SVGEllipse(centre, radiuses, attrs);
-    this.items.add(name, ellipse);
-    return ellipse;
-  }
-
-  /**
-   * Create path.
-   * @param {string} name - Item name.
-   * @param {string} value - Path commands value.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createPath(name, value, attrs) {
-    let path = new SVGPath(value, attrs);
-    this.items.add(name, path);
-    return path;
-  }
-
-  /**
-   * Create polygon.
-   * @param {string} name - Item name.
-   * @param {number[][]|Point[]} points - Array of (x, y) pairs or Point instances.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createPolygon(name, points, attrs) {
-    let polygon = new SVGPolygon(points, attrs);
-    this.items.add(name, polygon);
-    return polygon;
-  }
-
-  /**
-   * Create polyline.
-   * @param {string} name - Item name.
-   * @param {number[][]|Point[]} points - Array of (x, y) pairs or Point instances.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createPolyline(name, points, attrs) {
-    let polyline = new SVGPolyline(points, attrs);
-    this.items.add(name, polyline);
-    return polyline;
-  }
-
-  /**
-   * Create text.
-   * @param {string} name - Item name.
-   * @param {string} value - Value.
-   * @param {Point|number[]} centre - Point instatce or (x, y) pair.
-   * @param {Object} [attrs] - SVG element attributes.
-   * @param {string} [attrs.fill="none"] - SVG element fill value.
-   * @param {string} [attrs.stroke="#000000"] - SVG element stroke value.
-   */
-  createText(name, value, centre, attrs) {
-    let text = new SVGText(value, centre, attrs);
-    this.items.add(name, text);
-    return text;
   }
 
   /**
